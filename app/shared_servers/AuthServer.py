ba0a8cbfee3f54b1651d2b735e9ebaafc3112a26
Cecilia Hortas <37588761+chortas@users.noreply.github.com>
2020-05-23 13:48:44 -0300

    Mock auth server

--- app/shared_servers/AuthServer.py
+++ app/shared_servers/AuthServer.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
-import flask
 import json
-import requests
+import flask
+from shared_servers.utils_auth import *
 
 class AuthServer():
 
@@ -9,43 +9,71 @@ class AuthServer():
         self.url = "some_url"
         self.active_sessions = {}
 
-    def login(self, credentials, password):
+    def login(self, data):
         raise Exception('Not implemented yet')
 
-    def register(self, registration_data):
+    def register(self, data):
         raise Exception('Not implemented yet')
 
-    def validate_token(self, token):
-        if self.token_active(token):
-            return True
-        
-        # Should request Auth Server to check if it's still valid
+    def get_users(self):
         raise Exception('Not implemented yet')
 
-    def token_active(self, token):
-        return token in self.active_sessions and self.active_sessions[token]["ttl"] > datetime.now()
-
+    def authorize_user(self, data):
+        # Should request Auth Server to check if token is still valid
+        raise Exception('Not implemented yet')
 
 # --- Mocks
 
 class MockAuthServer(AuthServer):
-
     def __init__(self):
         super().__init__()
-        self.active_sessions['token1'] = { 'username': 'user1', 'ttl': datetime.now() + timedelta(days=3) }
-        self.active_sessions['token2'] = { 'username': 'user2', 'ttl': datetime.now() + timedelta(days=3) }
-        self.active_sessions['token3'] = { 'username': 'user3', 'ttl': datetime.now() + timedelta(days=3) }
+        self.db = {}
+        init_db(self.db)
+        self.next_id = len(self.db)
 
-    def login(self, credentials, password):
-        for k, v in self.active_sessions.items():
-            if v['username'] == credentials:
-                response_data = { 'userId': '1', 'token': k }
-                return flask.Response(json.dumps(response_data), status=200)
+    def login(self, data):
+        parsed_data = json.loads(data)
+        email = parsed_data['email']
+        password = parsed_data['password']
+        if email not in self.db:
+            return flask.Response('Could not find user', status=401)
+        if not check_password_hash(password, self.db[email]['password']):
+            return flask.Response('Password incorrect', status=401)
+        
+        user = self.db[email]
+        response_data = {'token': get_token(email), 'status': 'OK', 'user': get_fields(user)}
+        return flask.Response(json.dumps(response_data), status=200)
+    
+    def generate_id(self):
+        self.next_id += 1
+        return self.next_id
 
-        return flask.Response('', status=400)
+    def register(self, data):
+        parsed_data = json.loads(data)
+        email = parsed_data["email"] 
+        username = parsed_data['username'] 
+        password = parsed_data['password']
+        hashed_password = get_hash(password)
+        if email in self.db or any(user['username'] == username for user in self.db.values()):
+            return flask.Response('User already registered', status=409)
+        if not validate(email):
+            return flask.Response('Invalid email address', status=400)
+        id = self.generate_id()
+        self.db[email] = {'id': id, 'email': email, 'password': hashed_password, 'username': username}
+        response_data = {'id': id}
+        return flask.Response(json.dumps(response_data), status=200)
 
-    def register(self, registration_data):
-        return flask.Response('User registered', status=201)
+    def get_users(self):
+        response_data = list(map(lambda user: get_fields(user), self.db.values()))
+        return flask.Response(json.dumps(response_data), status=200)
 
-    def validate_token(self, token):
-        return token_active(token)
+    def authorize_user(self, data):
+        parsed_data = json.loads(data)
+        token = parsed_data['token']
+        email = get_email(token)
+        if email not in self.db:
+            return flask.Response("Invalid Token", status=401)
+        user = self.db[email]
+        response_data = {'status':'OK', 'user': get_fields(user)}
+        return flask.Response(json.dumps(response_data), status=200)
+
---
+++ app/tests/test_mock_auth.py
@@ -0,0 +1,106 @@
+import pytest
+from json import loads, dumps
+from shared_servers.AuthServer import MockAuthServer
+
+class TestMockAuthServer:
+    def setup_method(self, method):
+        """ setup any state tied to the execution of the given method in a
+        class.  setup_method is invoked for every test method of a class.
+        """
+        self.mock_auth_server = MockAuthServer()
+
+    def test_register_success(self):
+        """ Register an user should return 200 """
+
+        registration_data = dumps({'email': 'email@gmail.com', 'username': 'theboss', 'password': '123'})
+        response = self.mock_auth_server.register(registration_data)
+        json = loads(response.get_data())
+        assert json['id'] == 5
+        assert response.status_code == 200
+
+    def test_register_failure_invalid_email(self):
+        """ Register an user should return 400 """
+
+        registration_data = dumps({'email': 'wrong_email', 'username': 'theboss', 'password': '123'})
+        response = self.mock_auth_server.register(registration_data)
+        assert b'Invalid email address' in response.get_data()
+        assert response.status_code == 400
+
+    def test_register_failure_username_taken(self):
+        """ Register an user with username taken should return 400 """
+
+        registration_data = dumps({'email': 'email@gmail.com', 'username': 'theboss', 'password': '123'})
+        self.mock_auth_server.register(registration_data)
+        other_registration_data = dumps({'email': 'other_email@gmail.com', 'username': 'theboss', 'password': '123'})
+        response = self.mock_auth_server.register(other_registration_data)
+        assert b'User already registered' in response.get_data()
+        assert response.status_code == 409
+    
+    def test_login_success(self):
+        """ User login should return 200 and a token """
+
+        registration_data = dumps({'email': 'email@gmail.com', 'username': 'theboss', 'password': '123'})
+        self.mock_auth_server.register(registration_data)
+        login_data = dumps({'email': 'email@gmail.com', 'password': '123'})
+        response = self.mock_auth_server.login(login_data)
+        json = loads(response.get_data())
+        assert 'token' in json
+        assert json['user']['username'] == 'theboss'
+        assert json['user']['email'] == 'email@gmail.com'
+        assert response.status_code == 200
+
+    def test_login_failure_password(self):
+        """ User login with wrong password should return 401 """
+
+        registration_data = dumps({'email': 'email@gmail.com', 'username': 'theboss', 'password': '123'})
+        self.mock_auth_server.register(registration_data)
+        login_data = dumps({'email': 'email@gmail.com', 'password': 'wrong'})
+        response = self.mock_auth_server.login(login_data)
+        assert b'Password incorrect' in response.get_data()
+        assert response.status_code == 401
+
+    def test_login_failure_inexistent(self):
+        """ Login of unknown user should return 401 """
+
+        login_data = dumps({'email': 'email@gmail.com', 'password': 'wrong'})
+        response = self.mock_auth_server.login(login_data)
+        assert b'Could not find user' in response.get_data()
+        assert response.status_code == 401
+
+    def test_authorize_success(self):
+        """ Authorize a user with valid token should return 200 """
+
+        registration_data = dumps({'email': 'email@gmail.com', 'username': 'theboss', 'password': '123'})
+        self.mock_auth_server.register(registration_data)
+        login_data = dumps({'email': 'email@gmail.com', 'password': '123'})
+        response = self.mock_auth_server.login(login_data)        
+        json = loads(response.get_data())
+        token = json['token']
+        authorize_data = dumps({'token': token})
+        response = self.mock_auth_server.authorize_user(authorize_data)
+        json = loads(response.get_data())
+        assert json['user']['email'] == 'email@gmail.com'
+        assert json['user']['username'] == 'theboss'
+        assert response.status_code == 200
+
+    def test_authorize_failure_invalid(self):
+        """ Authorize a user with invalid token should return 401 """
+
+        authorize_data = dumps({'token': 'invalid_token'})
+        response = self.mock_auth_server.authorize_user(authorize_data)
+        assert b'Invalid Token' in response.get_data()
+        assert response.status_code == 401
+
+    def test_get_users_success(self):
+        """ Get users should return 200 with user data """
+
+        response = self.mock_auth_server.get_users()
+        json = loads(response.get_data())
+        assert response.status_code == 200
+        id = 1
+        for user in json:
+            assert user['id'] == str(id)
+            assert user['email'] == 'email' + str(id)
+            assert user['username'] == 'user' + str(id)
+            id += 1
+
