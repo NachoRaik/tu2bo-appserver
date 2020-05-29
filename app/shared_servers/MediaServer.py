e1e5df6636f0925bfa71c721e9d46f376616c6ca
chortas <chortas@fi.uba.ar>
2020-05-29 19:08:46 -0300

    Add more test to add_video cases

--- app/shared_servers/MediaServer.py
+++ app/shared_servers/MediaServer.py
@@ -25,7 +25,6 @@ class MediaServer():
 # --- Mocks
 
 class MockMediaServer(MediaServer):
-
     def __init__(self):
         super().__init__()
         self.db = {}
@@ -40,14 +39,16 @@ class MockMediaServer(MediaServer):
 
     def add_video(self, data):
         parsed_data = json.loads(data)
-        if any(video['url'] == parsed_data['url'] for video in self.db_metadata.values()):
+        url, author, title, visibility = parsed_data['url'], parsed_data['author'], parsed_data['title'], parsed_data['visibility']
+        description = parsed_data['description'] if 'description' in data else ''
+        thumb = parsed_data['thumb'] if 'thumb' in data else ''
+        if any(video['url'] == url for video in self.db_metadata.values()):
             return flask.Response('Video already uploaded', status=409)
         date = datetime.strptime(parsed_data['date'], '%m/%d/%y %H:%M:%S')
         if date > datetime.now() or not validate_visibility(parsed_data['visibility']):
             return flask.Response('Invalid date', status=400)
         id = self.generate_id()
-        self.db[id] = {'author': parsed_data['author'], 'title': parsed_data['title'], 'description': parsed_data['description'], 
-                    'date': parsed_data['date'], 'visibility': parsed_data['visibility']}
-        self.db_metadata[id] = {'url': parsed_data['url'], 'thumb': parsed_data['thumb']}
+        self.db[id] = {'author': author, 'title': title, 'description': description, 'date': date, 'visibility': visibility}
+        self.db_metadata[id] = {'url': url, 'thumb': thumb}
         response_data = {'id': id}
         return flask.Response(json.dumps(response_data), status=200)
--- app/tests/test_mock_media.py
+++ app/tests/test_mock_media.py
@@ -10,9 +10,28 @@ class TestMockMediaServer:
         """
         self.mock_media_server = MockMediaServer()
 
-    def test_add_video_full_datasuccess(self):
-        """ Register an user should return 200 """
-        video_data = dumps({'author': 'anAuthor', 'title': 'aTitle', 'description': 'aDescription', 'date': '09/19/18 13:55:26', 'visibility': 'public', 'url': 'anUrl', 'thumb': 'aThumb'})
+    def test_add_video_full_data_success(self):
+        """ Add a video should return 200 """
+        video_data = dumps({'author': 'anAuthor', 'title': 'aTitle', 'description': 'aDescription', 'date': '09/19/18 13:55:26', 
+        'visibility': 'public', 'url': 'anUrl', 'thumb': 'aThumb'})
+        response = self.mock_media_server.add_video(video_data)
+        json = loads(response.get_data())
+        assert json['id'] == 5
+        assert response.status_code == 200
+    
+    def test_add_video_without_thumb_success(self):
+        """ Add a video should return 200 """
+        video_data = dumps({'author': 'anAuthor', 'title': 'aTitle', 'description': 'aDescription', 'date': '09/19/18 13:55:26', 
+        'visibility': 'public', 'url': 'anUrl'})
+        response = self.mock_media_server.add_video(video_data)
+        json = loads(response.get_data())
+        assert json['id'] == 5
+        assert response.status_code == 200
+
+    def test_add_video_without_description_success(self):
+        """ Add a video should return 200 """
+        video_data = dumps({'author': 'anAuthor', 'title': 'aTitle', 'date': '09/19/18 13:55:26', 'visibility': 'public', 
+        'url': 'anUrl', 'thumb': 'aThumb'})
         response = self.mock_media_server.add_video(video_data)
         json = loads(response.get_data())
         assert json['id'] == 5
