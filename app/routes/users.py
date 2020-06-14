1ad1cf64c3f8f944c7dc703aae1b8ea4e91554d5
chortas <chortas@fi.uba.ar>
2020-06-14 15:49:58 -0300

    Endpoints implemented. Missing tests

--- app/routes/users.py
+++ app/routes/users.py
@@ -24,6 +24,9 @@ def user_videos(user_id):
                 return Response(json.dumps({'reason':'Fields are incomplete'}), status=400) 
         
         response = media_server.add_video(body)
+        response_data = loads(response.get_data())
+        video_id = response_data['id']
+        video_info = VideoInfo(video_id=video_id).save()
         return response
     else:
         videos = media_server.get_user_videos(userId)
--- app/routes/videos.py
+++ app/routes/videos.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
 from flask import current_app as app
 from werkzeug.utils import secure_filename
 from database.models.video_info import VideoInfo
@@ -8,6 +8,7 @@ from database.models.comment import Comment
 bp_videos = Blueprint("bp_videos", __name__)
 
 required_post_comment_fields = ['author', 'content', 'timestamp']
+required_put_likes_field = 'liked'
 
 # -- Endpoints
 
@@ -17,53 +18,68 @@ def home_videos():
     home_page_videos = media_server.get_videos()
     return home_page_videos
 
+def add_comment_to_video(request, video_id):
+    body = request.get_json()
+    
+    if any(r not in body for r in required_post_comment_fields):
+        return Response(json.dumps({'reason':'Fields are incomplete'}), status=400) 
+
+    response = media_server.get_video(video_id)
+    if response.status == 404:
+        return Response(json.dumps({'reason':'Video not found'}), status=404) 
+
+    #TODO: add user_id obtained by the auth server. For now it is harcoded
+    user_id = 1
+
+    author, content, timestamp = body['author'], body['content'], body['timestamp']
+    comment = Comment(author=author, user_id=user_id, content=content, timestamp=timestamp)
+    video_info = VideoInfo.objects.get(video_id=video_id)
+    video_info.comments.append(comment)
+    video_info.save()
+
+    result = {'comment_id': comment.comment_id, 'user_id': user_id, 'author': author, 'content': content, 'timestamp': timestamp}
+    return Response(json.dumps(result), status=200) 
+
+def get_comment_from_video(request, video_id):
+    response = media_server.get_video(video_id)
+    if response.status == 404:
+        return Response(json.dumps({'reason':'Video not found'}), status=404) 
+
+    body = request.get_json()
+    video_info = VideoInfo.objects.get(video_id=video_id)
+    comments = video_info.comments
+    
+    result = []
+    for comment in comments:
+        result.append({'comment_id': comment.comment_id, 'user_id': comment.user_id, 'author': comment.author, 
+        'content': comment.content, 'timestamp': comment.timestamp})
+    return Response(json.dumps(result), status=200) 
+
 @bp_videos.route('/videos/<video_id>/comments', methods=['GET', 'POST'])
 def video_comments(video_id):
     auth_server = app.config['AUTH_SERVER']
     media_server = app.config['AUTH_SERVER']
 
     if request.method == 'POST':
-        for r in required_post_comment_fields:
-            if r not in body:
-                return Response(json.dumps({'reason':'Fields are incomplete'}), status=400) 
+        return add_comment_to_video(request, video_id)
+    return get_comment_from_video(request, video_id)
 
-        body = request.get_json()
-        response = media_server.get_video(video_id)
-        if response.status == 404:
-            return Response(json.dumps({'reason':'Video not found'}), status=404) 
+@bp_videos.route('/videos/<video_id>/likes', methods=['PUT'])
+def video_likes(video_id):
+    media_server = app.config['AUTH_SERVER']
+    if required_put_likes_field not in body:
+        return Response(json.dumps({'reason':'Fields are incomplete'}), status=400) 
 
-        #TODO: add user_id obtained by the auth server. For now it is harcoded
-        user_id = 1
+    response = media_server.get_video(video_id)
+    if response.status == 404:
+        return Response(json.dumps({'reason':'Video not found'}), status=404) 
 
-        author, content, timestamp = body['author'], body['content'], body['timestamp']
-        comment = Comment(author=author, user_id=user_id, content=content, timestamp=timestamp)
+    liked = body['liked']
+    
+    #TODO: add user_id obtained by the auth server. For now it is harcoded
+    user_id = 1
 
-        video_info = VideoInfo(video_id=video_id)
-        try:
-            video_info = VideoInfo.objects.get(video_id=video_id)
-        except VideoInfo.DoesNotExist:
-            print("El video fue creado")
-
-        video_info.comments.append(comment)
-        video_info.save()
-
-        response = jsonify({'comment_id': comment.comment_id, 'user_id': user_id, 'author': author, 'content': content, 
-            'timestamp': timestamp})
-        response.status_code = 200
-        return response
-    else:
-        response = media_server.get_video(video_id)
-        if response.status == 404:
-            return Response(json.dumps({'reason':'Video not found'}), status=404) 
-
-        body = request.get_json()
-        video_info = VideoInfo.objects.get(video_id=video_id)
-        comments = video_info.comments
-        
-        result = []
-        for comment in comments:
-            result.append({'comment_id': comment.comment_id, 'user_id': comment.user_id, 'author': comment.author, 
-            'content': comment.content, 'timestamp': comment.timestamp})
-        response = jsonify(result)
-        response.status_code = 200
-        return response
+    video_info = VideoInfo.objects.get(video_id=video_id)
+    likes = video_info.likes
+    likes.append(user_id) if liked else likes.remove(user_id)
+    return Response(json.dumps({'result':'Like updated'}), status=200) 
