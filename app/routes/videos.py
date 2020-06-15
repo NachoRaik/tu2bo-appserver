d5c68087119cfbca3a49e3aa495b930a1d3cf428
matiscakosky <m>
2020-06-15 17:16:25 -0300

    very small fix

--- app/routes/videos.py
+++ app/routes/videos.py
@@ -9,7 +9,7 @@ bp_videos = Blueprint("bp_videos", __name__)
 # -- Endpoints
 
 @bp_videos.route('/videos', methods=['GET'])
-def home_videos(user_info):
+def home_videos():
     media_server = app.config['MEDIA_SERVER']
     home_page_videos = media_server.get_videos()
     return home_page_videos
