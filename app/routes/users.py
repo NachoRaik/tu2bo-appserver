3b06ecfd23cfeb256e5bf89a1650e5656ab57fc7
matiscakosky <m>
2020-06-21 01:29:32 -0300

    New endpoint my_requests

--- app/routes/users.py
+++ app/routes/users.py
@@ -15,6 +15,27 @@ required_post_video_fields = ['url', 'author', 'title', 'visibility', 'user_id']
 
 # -- Endpoints
 
+@bp_users.route('/users/my_requests', methods=['GET'])
+@token_required
+def user_pending_requests(user_info):
+    auth_server = app.config['AUTH_SERVER']
+    user_id = user_info["id"]
+    pending = PendingRequest.objects.with_id(user_id)
+    if pending is None:
+        error_response(404,'User pending requests not found')
+
+    response_data = []
+    pending_list = pending['requests']
+    for p_id in pending_list:
+        app.logger.debug("/userId=%s || Sending request to AuthServer",p_id)
+        response = auth_server.get_user_profile(p_id)
+        app.logger.debug("/userId=%s || Auth Server response %d %s ", p_id, response.status_code, response.data)
+        if response.status_code != 200: continue
+        pending_info = json.loads(response.get_data())
+        response_data.append({"id":pending_info["id"], "username":pending_info["username"]})
+    return success_response(200,response_data)
+
+
 @bp_users.route('/users/<user_id_request>', methods=['GET'])
 @token_required
 def get_user_profile(user_info, user_id_request):
@@ -38,8 +59,8 @@ def get_user_profile(user_info, user_id_request):
         if user_id_request in friends_list:
             status = 'friends'
     response_data = json.loads(response.get_data())
-    response_data["friendship_status"] = status
-    return success_response(200,response_data)
+    response_data['friendship_status'] = status
+    return success_response(200, response_data)
 
 @bp_users.route('/users/<int:user_id>/videos', methods=['GET', 'POST'])
 @token_required
