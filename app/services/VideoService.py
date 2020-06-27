bd7e3c8d56d7894630bc6a02e6bf37524cbdd5f3
Nacho Raik <ignacioraiksg@gmail.com>
2020-06-27 02:24:57 -0300

    VideoInfo handler added

--- app/services/VideoService.py
+++ app/services/VideoService.py
@@ -1,11 +1,10 @@
 import json
 from datetime import datetime
-from database.models.comment import Comment
-from database.models.video_info import VideoInfo
+from database.daos.VideoInfoDAO import VideoInfoDAO
 from utils.flask_utils import error_response
 
 class VideoService(object):
-    def __init__(self, media_server, db_handler=None):
+    def __init__(self, media_server, db_handler=VideoInfoDAO()):
         self.media_server = media_server
         self.db_handler = db_handler
     
@@ -13,9 +12,7 @@ class VideoService(object):
         res = self.media_server.get_videos()
         videos = json.loads(res.get_data())
         for video in videos:
-            video_id = video['id']
-            video_info = VideoInfo.objects.get(video_id=video_id)
-            video['likes'] = len(video_info.likes)
+            video['likes'] = len(self.db_handler.get_video_likes(video['id']))
         return videos
     
     def getVideo(self, user_id, video_id):
@@ -24,50 +21,35 @@ class VideoService(object):
             return None, res
         
         video = json.loads(res.get_data())
-        video_id = video['id']
-        video_info = VideoInfo.objects.get(video_id=video_id)
-        video['likes'] = len(video_info.likes)
-        video['user_related_info'] = {'is_liked': user_id in video_info.likes}
+        likes = self.db_handler.get_video_likes(video['id'])
+        video['likes'] = len(likes)
+        video['user_related_info'] = {'is_liked': user_id in likes}
         return video, None
 
     def addCommentToVideo(self, user_id, video_id, fields):
-        video = VideoInfo.objects.with_id(video_id)
-        if video is None:
+        comment = self.db_handler.add_video_comment(video_id, user_id, fields)
+        if comment is None:
             return None, error_response(404, 'Video not found')
 
-        author, content, timestamp = fields['author'], fields['content'], fields['timestamp']
-        comment = Comment(author=author, user_id=user_id, content=content, timestamp=timestamp)
-        video_info = VideoInfo.objects.get(video_id=video_id)
-        video_info.comments.append(comment)
-        video_info.save()
-
-        result = {'comment_id': comment.comment_id, 'user_id': user_id, 'author': author, 'content': content, 'timestamp': timestamp}
+        result = comment.to_mongo().to_dict()
+        del result['_id']
         return result, None
 
     def getCommentsFromVideo(self, video_id):
-        video = VideoInfo.objects.with_id(video_id)
-        if video is None:
+        comments = self.db_handler.get_video_comments(video_id)
+        if comments is None:
             return None, error_response(404, 'Video not found')
 
-        video_info = VideoInfo.objects.get(video_id=video_id)
-        comments = video_info.comments
         result = []
         for comment in comments:
             result.append({'comment_id': comment.comment_id, 'user_id': comment.user_id, 'author': comment.author,
-            'content': comment.content, 'timestamp': comment.timestamp})
+                           'content': comment.content, 'timestamp': comment.timestamp})
         result.sort(key=lambda d: datetime.strptime(d['timestamp'], '%m/%d/%y %H:%M:%S'))
         return result, None
 
-    def addLikeToVideo(self, user_id, video_id, liked):
-        video = VideoInfo.objects.with_id(video_id)
-        if video is None:
+    def addLikeToVideo(self, user_id, video_id, has_liked):
+        likes = self.db_handler.change_user_like_on_video(video_id, user_id, has_liked)
+        if likes is None:
             return error_response(404, 'Video not found')
 
-        video_info = VideoInfo.objects.get(video_id=video_id)
-        likes = video_info.likes
-        if not liked and user_id in likes:
-            likes.remove(user_id)
-        if liked and not user_id in likes:
-            likes.append(user_id)
-        video_info.save()
         return None
