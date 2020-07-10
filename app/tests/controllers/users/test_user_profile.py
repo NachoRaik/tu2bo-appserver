68760b1bdc3ea93b0266dc8b0f403fcd4a59c835
chortas <chortas@fi.uba.ar>
2020-07-10 19:21:00 -0300

    Delete friends and requests as well

--- app/tests/controllers/users/test_user_profile.py
+++ app/tests/controllers/users/test_user_profile.py
@@ -6,6 +6,7 @@ import json
 
 USER_1 = 1
 USER_2 = 2
+USER_3 = 3
 
 class TestUsersProfile:
     def setup_method(self, method):
@@ -21,8 +22,8 @@ class TestUsersProfile:
         db = _get_db()
         db.drop_collection('friends')
         db.drop_collection('pending_request')
+        db.drop_collection('video_info')
         disconnect(alias='test')
-
 
     # -- Users management
 
@@ -141,6 +142,98 @@ class TestUsersProfile:
         assert res.status_code == 200
         video_ids = [video['id'] for video in res_json]
         assert len(video_ids) == 0
+
+    def test_friends_deleted_from_user_profile_successfully(self, client):
+        """ DELETE /users/user_id
+        Should: return 204 """
+
+        token_user_1 = login_and_token_user(client, USER_1)
+        token_user_2 = login_and_token_user(client, USER_2)
+        token_user_3 = login_and_token_user(client, USER_3)
+
+        # Making friends
+        send_friend_request(client, token_user_1, USER_2)
+        accept_friend_request(client, token_user_2, USER_1)
+        send_friend_request(client, token_user_1, USER_3)
+        accept_friend_request(client, token_user_3, USER_1)
+
+        # Check that they are friends
+        res = get_user_friends(client, token_user_2, USER_2)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        friend_ids = [int(friend['id']) for friend in res_json]
+        assert friend_ids == [USER_1]
+
+        res = get_user_friends(client, token_user_3, USER_3)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        friend_ids = [int(friend['id']) for friend in res_json]
+        assert friend_ids == [USER_1]
+
+        # Delete user and its friends
+        res = delete_user_profile(client, token_user_1, USER_1)
+        assert res.status_code == 204
+
+        # Friends no longer exist
+        res = get_user_friends(client, token_user_2, USER_2)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        friend_ids = [int(friend['id']) for friend in res_json]
+        assert len(friend_ids) == 0
+
+        res = get_user_friends(client, token_user_3, USER_3)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        friend_ids = [int(friend['id']) for friend in res_json]
+        assert len(friend_ids) == 0
+
+        res = get_user_friends(client, token_user_1, USER_1)
+        assert res.status_code == 401
+
+    def test_friend_requests_deleted_from_user_profile_successfully(self, client):
+        """ DELETE /users/user_id
+        Should: return 204 """
+
+        token_user_1 = login_and_token_user(client, USER_1)
+        token_user_2 = login_and_token_user(client, USER_2)
+        token_user_3 = login_and_token_user(client, USER_3)
+
+        # Sending requests
+        send_friend_request(client, token_user_1, USER_2)
+        send_friend_request(client, token_user_1, USER_3)
+
+        # Check that they have the request
+        res = my_requests(client, token_user_2)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        request_ids = [int(request['id']) for request in res_json]
+        assert request_ids == [USER_1]
+
+        res = my_requests(client, token_user_3)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        request_ids = [int(request['id']) for request in res_json]
+        assert request_ids == [USER_1]
+
+        # Delete user and its requests
+        res = delete_user_profile(client, token_user_1, USER_1)
+        assert res.status_code == 204
+
+        # Friends no longer exist
+        res = my_requests(client, token_user_2)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        request_ids = [int(request['id']) for request in res_json]
+        assert len(request_ids) == 0
+
+        res = my_requests(client, token_user_3)
+        res_json = json.loads(res.get_data())
+        assert res.status_code == 200
+        request_ids = [int(request['id']) for request in res_json]
+        assert len(request_ids) == 0
+
+        res = my_requests(client, token_user_1)
+        assert res.status_code == 401
 
     def test_delete_unauthorized_user_profile(self, client):
         """ DELETE /users/user_id
